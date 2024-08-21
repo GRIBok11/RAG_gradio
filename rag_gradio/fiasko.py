414fd9f7dc1113acb6053445d35310dfc7d02eac
GRIBok11 <162333696+GRIBok11@users.noreply.github.com>
2024-08-21 10:13:55 +0300

    Delete rag_gradio directory

--- rag_gradio/fiasko.py
+++
@@ -1,9 +0,0 @@
-import mimetypes
-
-file_path = "moo2_manual.pdf"
-mime_type, encoding = mimetypes.guess_type(file_path)
-
-if mime_type is not None:
-    print(f"Тип файла: {mime_type}")
-else:
-    print("Не удалось определить тип файла")
